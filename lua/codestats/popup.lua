7812b5aa9f56e6c76ccc0ec088153c669a6bc56a
Miguel Carneiro <mcarneiromorenas@gmail.com>
2022-09-05 22:33:50 +0100

    initial commit

---
+++ lua/codestats/popup.lua
@@ -0,0 +1,15 @@
+local ok, popup = require("plenary.popup")
+
+if not ok then
+	print("plenary not present")
+	return
+end
+
+local M={}
+
+M.create_default_popup=function()
+	local win_id = popup.create({"menu 1", "menu 2"})
+	print(win_id)
+end
+
+return M
