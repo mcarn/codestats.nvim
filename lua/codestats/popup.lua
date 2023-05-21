c3f0e39abbfae62108952222273b5c2a7e62bd6f
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-21 08:31:17 +0100

    rename back

--- lua/codestats/popup.lua
+++ lua/codestats/popup.lua
@@ -3,7 +3,8 @@ local popup = require("plenary.popup")
 local function create_default_popup()
     local win_id = popup.create(
         { "menu 1", "menu 2" },
-        { line = 15, col = 45, minwidth = 20, border = true, highlight = PopupColor }
+        {title="Code::Stats",
+		relative="editor", col = 0, minwidth = 20, border = true, highlight = PopupColor }
     )
     print(win_id)
 end
