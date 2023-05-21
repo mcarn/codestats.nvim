21921710268bcb8ec5817487df48e7c4be6d387e
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-21 09:13:04 +0100

    remove docs

--- lua/codestats/request.lua
+++ lua/codestats/request.lua
@@ -1,16 +1,7 @@
 local curl = require("plenary.curl")
 
---- request module
--- @module request
--- @alias M
-
 local M = {}
 
---- fetches the username data
--- @param version codestats version
--- @param url codestats url
--- @param username codestats username
--- @return a table of all user data
 M.fetch = function(version, url, username)
     local res = curl.get(url .. "/users/" .. username, {
         accept = "application/json",
@@ -21,12 +12,6 @@ M.fetch = function(version, url, username)
     return vim.json.decode(res.body)
 end
 
---- pushes the username data
--- @param key codestats api key
--- @param version codestats version
--- @param url codestats url
--- @param payload user data
--- @return ok object
 M.push = function(key, version, url, payload)
     local res = curl.post(url .. "/my/pulses", {
         accept = "application/json",
