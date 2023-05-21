649dde8eb3b5f62386c2dd9cf9ed414a6999db93
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-21 08:57:08 +0100

    format

--- lua/codestats/request.lua
+++ lua/codestats/request.lua
@@ -1,29 +1,35 @@
-local curl = require'plenary.curl'
+local curl = require("plenary.curl")
 
 local M = {}
 
 M.fetch = function(version, url, username)
-
-	local res = 	curl.get(        url .. "/users/" .. username,{
-		accept = "application/json",
-		headers = {
-["Content-Type"]= "application/json"
-		}
-	})
-return vim.json.decode(res.body)
+    local res = curl.get(url .. "/users/" .. username, {
+        accept = "application/json",
+        headers = {
+            ["Content-Type"] = "application/json",
+        },
+    })
+    return vim.json.decode(res.body)
 end
 
 M.curl = function(key, version, url, payload)
     local cmd = {
         "curl",
-        "--header", "Content-Type: application/json",
-        "--header", "X-API-Token: " .. key,
-        "--user-agent", "codestats.nvim/" .. version,
-        "--data", payload,
-        "--request", "POST",
+        "--header",
+        "Content-Type: application/json",
+        "--header",
+        "X-API-Token: " .. key,
+        "--user-agent",
+        "codestats.nvim/" .. version,
+        "--data",
+        payload,
+        "--request",
+        "POST",
         "--silent",
-        "--output", "/dev/null",
-        "--write-out", "%{http_code}",
+        "--output",
+        "/dev/null",
+        "--write-out",
+        "%{http_code}",
         url .. "/my/pulses",
     }
 
@@ -31,7 +37,3 @@ M.curl = function(key, version, url, payload)
 end
 
 return M
-
-
-
-
