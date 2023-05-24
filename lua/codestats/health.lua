f1597102bc4810e18ae8de1090d8ef6d79107eac
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-24 22:47:08 +0100

    missing info

--- lua/codestats/health.lua
+++ lua/codestats/health.lua
@@ -1,5 +1,6 @@
 local start = vim.health.start or vim.health.report_start
 local ok = vim.health.ok or vim.health.report_ok
+local info = vim.health.info or vim.health.report_info
 local warn = vim.health.warn or vim.health.report_warn
 local error = vim.health.error or vim.health.report_error
 
