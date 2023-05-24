01d633ebb0b71ff63f8383e9933b15d51cb04284
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-24 21:43:39 +0100

    updated health (#36)

--- lua/codestats/health.lua
+++ lua/codestats/health.lua
@@ -1,17 +1,14 @@
-local health = vim.health
-local env = vim.env
-
 local M = {}
 
 local function check_setup()
     local opts = {}
 
-    local codestats_api_key = env.CODESTATS_API_KEY
+    local codestats_api_key = vim.env.CODESTATS_API_KEY
     if codestats_api_key == nil then
         table.insert(opts, "key")
     end
 
-    local username = env.CODESTATS_USERNAME
+    local username = vim.env.CODESTATS_USERNAME
     if username == nil then
         table.insert(opts, "username")
     end
@@ -27,21 +24,21 @@ local function empty(table)
     return next(table)
 end
 
-function M.check ()
-    health.start("codestats report")
+function M.check()
+    vim.health.start("codestats report")
 
     local status = check_setup()
 
-    if contains(status, "key") then
-        health.error("Missing CODESTATS_API_KEY")
-    end
-
-    if contains(status, "username") then
-        health.error("Setup is correct")
-    end
-
     if empty(status) then
-        health.ok("Setup is correct")
+        vim.health.ok("Setup is correct")
+    else
+        if contains(status, "key") then
+            vim.health.error("Missing CODESTATS_API_KEY")
+        end
+
+        if contains(status, "username") then
+            vim.health.error("Missing CODESTATS_USERNAME")
+        end
     end
 end
 
