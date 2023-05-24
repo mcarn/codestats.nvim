637a992a177b77bc8563e776e494740102500b47
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-24 22:20:36 +0100

    Refactor (#37)
    * renamed to init.lua
    
    * fix health

--- lua/codestats/health.lua
+++ lua/codestats/health.lua
@@ -1,16 +1,27 @@
 local M = {}
 
+local health = {
+    start = vim.health.start,
+    ok = vim.health.ok,
+    warn = vim.health.warn,
+    error = vim.health.error,
+    info = vim.health.info,
+}
+
+local key = "key"
+local username = "username"
+
 local function check_setup()
     local opts = {}
 
     local codestats_api_key = vim.env.CODESTATS_API_KEY
     if codestats_api_key == nil then
-        table.insert(opts, "key")
+        table.insert(opts, key)
     end
 
     local username = vim.env.CODESTATS_USERNAME
     if username == nil then
-        table.insert(opts, "username")
+        table.insert(opts, username)
     end
 
     return opts
@@ -25,19 +36,19 @@ local function empty(table)
 end
 
 function M.check()
-    vim.health.start("codestats report")
+    health.start("codestats report")
 
     local status = check_setup()
 
     if empty(status) then
-        vim.health.ok("Setup is correct")
+        health.ok("Setup is correct")
     else
-        if contains(status, "key") then
-            vim.health.error("Missing CODESTATS_API_KEY")
+        if contains(status, key) then
+            health.error("Missing CODESTATS_API_KEY")
         end
 
-        if contains(status, "username") then
-            vim.health.error("Missing CODESTATS_USERNAME")
+        if contains(status, username) then
+            health.error("Missing CODESTATS_USERNAME")
         end
     end
 end
